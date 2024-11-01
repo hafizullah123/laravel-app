46c435386069b5fb26e7c49aee2cec784cbe27b1
Hafizullah <hjihadwal@gmail.com>
2024-11-01 09:06:40 -0700

    change the route

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 use App\Http\Controllers\RegisterController; 
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layout');
-});
+// Route::get('/', function () {
+//     return view('layout');
+// });
  
 
 Route::resource('registers', RegisterController::class);
